965206017b58359103569c3184f8bdab53059509
Matt Borgerson <contact@mborgerson.com>
2020-02-19 00:16:06 -0700

    apu: Update file copyrights

--- hw/xbox/mcpx_apu.c
+++ hw/xbox/mcpx_apu.c
@@ -3,6 +3,7 @@
  *
  * Copyright (c) 2012 espes
  * Copyright (c) 2018-2019 Jannik Vogel
+ * Copyright (c) 2020 Matt Borgerson
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
